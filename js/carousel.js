f77a8bca6fdc6124ce75a0cc7a1a8b5b6887a8d6
Ana Amarante <kacaamarante@hotmail.com>
2025-05-21 08:39:53 -0300

    Adicionar o constructor para receber as informações e armazenar no array, criar a logica para adicionar a imagem e descrição no html

--- js/carousel.js
+++ js/carousel.js
@@ -5,11 +5,13 @@
 //Array storage class
 let carouselArr = [];
 
-
 //class Carousel
 class Carousel {
-
-    
+    constructor(image, title, url) {
+        this.Image = image;
+        this.Title = title;
+        this.Url = url;
+    }
       
     static Start(arr){
         if(arr){
@@ -27,6 +29,22 @@ class Carousel {
     }
 
     static Next(){
+        /*Definindo quem o item do array esta sendo selecionado, iniciando o que fica na posição
+        0 e somando de 1 em um ate 2 pois tem 3 itens*/
+        let item = carouselArr[Carousel._sequence];
         
+        const imagemCarousel = document.getElementById("carousel");
+        console.log(item.Image)
+        imagemCarousel.innerHTML = `<img src="./img/${item.Image}" style="height: 46vh;">`;
+
+        const tituloCarousel = document.getElementById("carousel-title");
+        console.log(item.Title)
+        tituloCarousel.innerHTML = `<a href="${item.Url}">${item.Title}</a>`;
+
+        Carousel._sequence++;
+        //Reiniciando a sequencia
+        if (Carousel._sequence >= Carousel._size) {
+            Carousel._sequence = 0;
+        }
     }
 };
